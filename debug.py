a77155d4213fe36cbb399fbf623f01611322372c
Andantei <zaoyinji@gmail.com>
2023-02-28 23:00:38 -0800

    update plugin to the latest format

--- debug.py
+++ debug.py
@@ -1,8 +1,7 @@
-# debug.py
-from __future__ import annotations
-
-from singing_transcription import TranscribeSinging
+from plugin import TranscribeSinging
 from tuneflow_devkit import Debugger
+from pathlib import Path
 
 if __name__ == "__main__":
-    Debugger(plugin_class=TranscribeSinging).start()
+    Debugger(plugin_class=TranscribeSinging, bundle_file_path=str(
+        Path(__file__).parent.joinpath('bundle.json').absolute())).start()
---
+++ plugin.py
@@ -0,0 +1,175 @@
+from __future__ import annotations
+
+from tuneflow_py import TuneflowPlugin, Song, ParamDescriptor, WidgetType, TrackType, InjectSource, Track, Clip
+from typing import Any
+from data_utils.seq_dataset import SeqDataset
+from predictor import EffNetPredictor
+import torch
+from io import BytesIO
+from pathlib import Path
+
+
+class TranscribeSinging(TuneflowPlugin):
+    @staticmethod
+    def provider_id():
+        return "hellwz"
+
+    @staticmethod
+    def plugin_id():
+        return "singing-transcription"
+
+    @staticmethod
+    def params(song: Song) -> dict[str, ParamDescriptor]:
+        return {
+            "clipAudioData": {
+                "displayName": {
+                    "zh": '音频',
+                    "en": 'Audio',
+                },
+                "defaultValue": None,
+                "widget": {
+                    "type": WidgetType.NoWidget.value,
+                },
+                "hidden": True,
+                "injectFrom": {
+                    "type": InjectSource.ClipAudioData.value,
+                    "options": {
+                        "clips": "selectedAudioClips"
+                    }
+                }
+            },
+            "selectedClipInfos": {
+                "displayName": {
+                    "zh": '选中片段',
+                    "en": 'Selected Clips',
+                },
+                "defaultValue": None,
+                "widget": {
+                    "type": WidgetType.NoWidget.value,
+                },
+                "hidden": True,
+                "injectFrom": {
+                    "type": InjectSource.SelectedClipInfos.value,
+                    "options": {
+                        "maxNumClips": 1
+                    }
+                }
+            },
+            "doSeparation": {
+                "displayName": {
+                    "zh": '自动分离伴奏',
+                    "en": 'Automatic accompaniment separation',
+                },
+                "defaultValue": False,
+                "description": {
+                    "zh": '先自动分离音频中的伴奏，再进行人声转录',
+                    "en": 'Automatically separate the accompaniment from the audio first, then transcribe the vocals',
+                },
+                "widget": {
+                    "type": WidgetType.Switch.value,
+                },
+            },
+            "onsetThreshold": {
+                "displayName": {
+                    "zh": '音符起始阈值',
+                    "en": 'Onset threshold',
+                },
+                "defaultValue": 0.4,
+                "description": {
+                    "zh": '该阈值越大，转录出的MIDI音符数越少',
+                    "en": 'The higher the threshold, the lower the number of MIDI notes that will be transcribed',
+                },
+                "widget": {
+                    "type": WidgetType.Slider.value,
+                    "config": {
+                        "minValue": 0.1,
+                        "maxValue": 0.9,
+                        "step": 0.1
+                    }
+                },
+            },
+            "silenceThreshold": {
+                "displayName": {
+                    "zh": '音符结束阈值',
+                    "en": 'Silence threshold',
+                },
+                "defaultValue": 0.5,
+                "description": {
+                    "zh": '该阈值越大，转录出的MIDI音符越长',
+                    "en": 'The higher the threshold, the longer the MIDI note transcribed',
+                },
+                "widget": {
+                    "type": WidgetType.Slider.value,
+                    "config": {
+                        "minValue": 0.1,
+                        "maxValue": 0.9,
+                        "step": 0.1
+                    }
+                },
+            }
+        }
+
+    @staticmethod
+    def run(song: Song, params: dict[str, Any]):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        predictor = EffNetPredictor(device=device, model_path=str(
+            Path(__file__).parent.joinpath("models").joinpath("1005_e_4").absolute()))
+        selected_clip_infos = params["selectedClipInfos"]
+        selected_clip_info = selected_clip_infos[0]
+        track = song.get_track_by_id(selected_clip_info["trackId"])
+        if track is None:
+            raise Exception("Cannot find track")
+        clip = track.get_clip_by_id(selected_clip_info["clipId"])
+        if clip is None:
+            raise Exception("Cannot find clip")
+        clip_audio_data_list = params["clipAudioData"]
+        new_midi_track = song.create_track(type=TrackType.MIDI_TRACK, assign_default_sampler_plugin=True)
+
+        TranscribeSinging._transcribe_clip(predictor, song,
+                                           new_midi_track,
+                                           clip,
+                                           BytesIO(clip_audio_data_list[0]["audioData"]["data"]),
+                                           params["doSeparation"],
+                                           params["onsetThreshold"],
+                                           params["silenceThreshold"])
+
+    @staticmethod
+    def _transcribe_clip(
+        predictor,
+        song: Song,
+        new_midi_track: Track,
+        audio_clip: Clip,
+        audio_file_path,
+        do_separation=False,
+        onset_threshold=0.4,
+        silence_threshold=0.5,
+    ):
+        new_clip = new_midi_track.create_midi_clip(
+            clip_start_tick=audio_clip.get_clip_start_tick(),
+            clip_end_tick=audio_clip.get_clip_end_tick(),
+            insert_clip=True
+        )
+        audio_start_tick = audio_clip.get_audio_clip_data().start_tick
+        audio_start_time = song.tick_to_seconds(audio_start_tick)
+        # TODO: Trim the audio so that we only transcribe the visible part.
+        test_dataset = SeqDataset(audio_file_path, song_id='1', do_svs=do_separation)
+
+        results = {}
+        results = predictor.predict(test_dataset, results=results,
+                                    onset_thres=onset_threshold, offset_thres=silence_threshold)
+
+        for notes in results['1']:
+            note_start_time_within_audio = notes[0]
+            note_start_tick = song.seconds_to_tick(note_start_time_within_audio + audio_start_time)
+            note_end_time_within_audio = notes[1]
+            note_end_tick = song.seconds_to_tick(note_end_time_within_audio + audio_start_time)
+            note_pitch = notes[2]
+
+            new_clip.create_note(
+                pitch=note_pitch,
+                velocity=100,
+                start_tick=note_start_tick,
+                end_tick=note_end_tick
+            )
+        new_clip.adjust_clip_left(clip_start_tick=audio_clip.get_clip_start_tick(), resolve_conflict=False)
+        new_clip.adjust_clip_right(clip_end_tick=audio_clip.get_clip_end_tick(), resolve_conflict=False)
