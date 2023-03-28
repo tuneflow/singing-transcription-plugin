b943ec33db6cc9f7029b3b027969cd6c4ce59994
Andantei <zaoyinji@gmail.com>
2023-03-28 14:27:11 -0700

    add plugin icon, update endpoint

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import uvicorn
 
 app = Runner(plugin_class_list=[TranscribeSinging], bundle_file_path=str(
-    Path(__file__).parent.joinpath('bundle.json').absolute())).start(path_prefix='/plugins/transcribe-singing')
+    Path(__file__).parent.joinpath('bundle.json').absolute())).start(path_prefix='/plugin-service/singing_transcription')
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
